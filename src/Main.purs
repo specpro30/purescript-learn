26d5bbbbc149d27bb24e83916e34b6620eacc531
specpro30 <specpro30@gmail.com>
2022-01-22 13:48:36 +0800

    pattern matching

--- src/Main.purs
+++ src/Main.purs
@@ -18,8 +18,15 @@ type Person =
   }
 
 birthday :: Person -> Person
-birthday person = 
-  person { age = person.age + 1 } 
+birthday { name, age } = 
+  { name: name, age: age + 1 } 
+
+f :: Int -> Int -> Int
+f 0 _ = -1 
+f _ y = 25 + y
+f 100 y = -50 + y
+f 0 50 = -500
+f x y = x + y
 
 main :: Effect Unit
 main =
@@ -27,4 +34,4 @@ main =
         joe = { name: "Joe", age: 39}
         joes_birthday = birthday joe
     in do
-    log "hello world 3" 
+    log "hello world 3.2" 
